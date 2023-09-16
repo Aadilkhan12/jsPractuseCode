2b7470ef48a52b31877919b358865996c0f56b14
Aadil <90678881+Aadilkhan12@users.noreply.github.com>
2023-09-16 18:26:39 +0530

    Create datatype.js

---
+++ concept1/datatype.js
@@ -0,0 +1,18 @@
+// primitive data  type 
+  let  age = 23 ; // number 
+   let  name ="Aadil" ; // string
+  let state  =   true ;  //  boolean  data type 
+  // number  =>  2   to power 32 
+ //   bigInt
+ // string => ""
+// boolean => true / false 
+// num => standalone value 
+// undefined  =>    if  varible decleared but not  initalize 
+// symbol  =>  unique 
+
+
+ // non primitive 
+
+// object
+// array
+// 
