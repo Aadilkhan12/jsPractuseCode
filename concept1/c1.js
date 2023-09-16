0450f1cc4f1457e33015be749cf25915a978d0d0
Aadil <90678881+Aadilkhan12@users.noreply.github.com>
2023-09-16 17:05:08 +0530

    Update c1.js

--- concept1/c1.js
+++ concept1/c1.js
@@ -1,1 +1,43 @@
 console.log("love")
+
+// Issues with var keyword
+ var  car =  "honda";
+var  car = "Maruti";
+console.log(car);    // Maruti
+
+for (car count =0 ; count <5; count++){
+console.log(count);     // 0 1  2 3 4
+  
+}
+ console.log(count)  // 5
+ car = "TATA";
+console.log(car); //TATA
+
+
+
+// a variable declared with let
+ let  count =0 ;
+count =1; 
+console.log(count ) ;  // 1
+  let count  = 2 ;   //Error : already Declared
+ if( true )
+ {
+var name = "bob";
+   let age = 12 ;
+   console.log(name+age);  //bob12
+ }
+console.log(name);   //bob
+console.log(age); // Error : age is not  defined 
+
+
+//const should declared and initialize at the same time
+ var   age ; 
+console.log(age);   // undefined 
+  let  address ; 
+console.log(address);   // undfined 
+ const  city;
+ console.log(city);   // Error 
+
+
+///We should make an habit to use const and let over var to avoid unnecessary confusion/errors.
+// https://medium.com/@aayushtibra1997
